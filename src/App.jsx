9f2a24d41a9340e9792339ae80b1c6665749cca3
Rukevwe Ukwa <rukevweukwa@gmail.com>
2025-10-14 23:24:17 +0100

    Refactor component structure and update dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold text-blue-600">
-      Hello from ver el mundo!
-    </h1>
-  )
+    <div className="font-sans">
+      <Navbar />
+      <Hero />
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
